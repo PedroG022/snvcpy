1e93f72b64165429f46798e107e5c4e1300b0788
PedroG022 <55058149+PedroG022@users.noreply.github.com>
2021-04-03 13:07:55 -0300

    Delete install-dependencies.ps1
    Actually unnecessary file and dependencies installation method.

--- snvcpy/install-dependencies.ps1
+++
@@ -1,8 +0,0 @@
-echo "Installing choco..."
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-echo "Testing choco"
-choco -?
-choco install adb
-choco install scrcpy
-echo "Everything should be alright now"
-pause
